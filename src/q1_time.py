90ba8af6dd5670ef0a39a00a65bf60e287797965
Enrique Olivares <>
2024-10-11 21:27:50 -0600

    feat: Formatted files

--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,6 @@
-from typing import List, Tuple
 from datetime import datetime
+from typing import List, Tuple
+
 
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
     pass
--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
 
+
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
     pass
